3cefb25031b818d6e863a6b16b0261011cf81d80
Isivara2021 <isivara2021@gmail.com>
2026-02-02 17:38:57 +0530

    Update contact us page map

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -73,13 +73,13 @@ const Contact = () => {
             {/* Map Embed */}
             <div className="map-container">
               <iframe
+                title="Location Map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.146324052157!2d80.1352139747589!3d7.109035892894425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3038ebe2497b3%3A0x3cc019a3e05ba32a!2sISIRO%20Steel%20Industries!5e0!3m2!1sen!2slk!4v1770033173190!5m2!1sen!2slk"
                 width="100%"
                 height="200"
-                style="border:0;"
+                style={{ border: 0 }}
                 allowfullscreen=""
                 loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
           </div>
